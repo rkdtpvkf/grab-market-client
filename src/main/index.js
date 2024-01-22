a6e07bcddfba0cdbbb56b28455b0a877e5cca5e6
Kang sae <rkdtpvkf@naver.com>
2024-01-22 17:25:15 +0900

    project init

--- src/main/index.js
+++ src/main/index.js
@@ -46,6 +46,7 @@ function MainPage() {
                       src="images/icons/avatar.png"
                     />
                     <span>{product.seller}</span>
+                    <span>테스트</span>
                   </div>
                 </div>
               </div>
