45031ec15194d8304ea7d0d92a43930427d97d50
rkdtpvkf <43945503+rkdtpvkf@users.noreply.github.com>
2024-01-22 17:36:21 +0900

    Update index.js
    test1

--- src/main/index.js
+++ src/main/index.js
@@ -46,8 +46,6 @@ function MainPage() {
                       src="images/icons/avatar.png"
                     />
                     <span>{product.seller}</span>
-                    <span>테스트</span>
-                    <span>test</span>    
                   </div>
                 </div>
               </div>
